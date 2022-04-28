bdc8ed3f708c4fe60db8853e12119d981c772898
janaka <bajpushpakumara@gmail.com>
2022-04-28 11:49:38 -0300

    CPU Spinning class mediator example for testing

--- Basics/Class-Mediator/src/main/java/com/sample/TestMediator.java
+++ Basics/Class-Mediator/src/main/java/com/sample/TestMediator.java
@@ -5,6 +5,8 @@ import org.apache.commons.logging.LogFactory;
 import org.apache.synapse.MessageContext;
 import org.apache.synapse.mediators.AbstractMediator;
 
+import javax.swing.plaf.IconUIResource;
+
 /**
  * This is a sample class mediator  that log for message flows.
  */
@@ -15,10 +17,21 @@ public class TestMediator extends AbstractMediator {
     private static final Log log = LogFactory.getLog(TestMediator.class);
 
     public boolean mediate(MessageContext messageContext) {
-        log.info("Class mediator invoked");
+        log.debug("Class mediator invoked");
+        spin(Integer.parseInt(messageContext.getProperty("Delay").toString()));
         return true;
     }
-
+    // CPU trap
+    private static void spin(int milliseconds) {
+        long sleepTime = milliseconds*1000000L; // convert to nanoseconds
+        long startTime = System.nanoTime();
+        while ((System.nanoTime() - startTime) < sleepTime) {
+            long count = 0l;
+            for(long x=0;x<Integer.MAX_VALUE ;x++){
+                count+=1;
+            }
+        }
+    }
     public String getType() {
         return null;
     }
